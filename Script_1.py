7dc76e0b4b9816fdc644b2c99ce00ddb31d14544
Maxim Kugayevskiy <167158972+MaxKuga@users.noreply.github.com>
2024-08-05 21:39:34 -0700

    Update Script_1.py

--- Script_1.py
+++ Script_1.py
@@ -3,3 +3,9 @@ import time
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get("https://ecommerce-playground.lambdatest.io/index.php?route=account/register")
+driver.maximize_window()
+driver.minimize_window()
+driver.maximize_window()
