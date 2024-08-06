e060297de43d45b43bf84ff78eebb796db1da7fa
MaxKuga <maxqa2020@gmail.com>
2024-08-05 21:40:30 -0700

    Test head script

--- Script_1.py
+++ Script_1.py
@@ -9,3 +9,23 @@ driver.get("https://ecommerce-playground.lambdatest.io/index.php?route=account/r
 driver.maximize_window()
 driver.minimize_window()
 driver.maximize_window()
+
+# filling in the form
+# first name
+driver.find_element(By.ID, "input-firstname").send_keys("Mike")
+
+# last name
+driver.find_element(By.ID, "input-lastname").send_keys("Smith")
+
+# add email
+driver.find_element(By.ID, "input-email").send_keys("myemail@example.com")
+
+# add new phone number
+driver.find_element(By.ID, "input-telephone").send_keys("123-456-7890")
+
+# add new password
+password = "qwerty123"
+driver.find_element(By.ID, "input-password").send_keys(password)
+
+# password_confirm
+driver.find_element(By.ID, "input-confirm").send_keys(password)
